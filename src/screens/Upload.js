e08f08c2dbebdfb57d85d0c258f4324b6396d70e
Sumeet Rohra <sumeetrohra1@gmail.com>
2018-09-25 18:04:02 +0530

    Upoad, DB, video Player done, fetching data and video url remaining.

--- src/screens/Upload.js
+++ src/screens/Upload.js
@@ -1,18 +1,111 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { NavigationActions } from 'react-navigation';
+import { View } from 'react-native';
+import { Button, FormInput, FormLabel } from 'react-native-elements';
+import { DocumentPicker } from 'expo';
+import firebase from 'firebase';
+import ModalActivityIndicator from 'react-native-modal-activityindicator'
 
 class Upload extends Component {
 
+    state = {
+        uploadButtonDisable: true,
+        result: null,
+        loading: false,
+        name: null,
+        url: null
+    }
+
+    initialState = () => {
+        this.setState({
+            uploadButtonDisable: true,
+            result: null,
+            loading: false,
+            name: null,
+            url: null
+        });
+    }
+
+    //document picker used for getting video uri
+    pickVideo = async () => {
+        let result = await DocumentPicker.getDocumentAsync({});
+        this.setState({ result: result, uploadButtonDisable: false });
+        console.log(this.state.result);
+        //console.log(firebase.auth().currentUser);
+        //this.uploadVideo(result);
+    }
+
+    //video upload
+    uploadVideo = async () => {
+        const result = this.state.result;
+        if (result && this.state.name) {
+            //fetching the video blob
+            this.setState({ loading: true });
+            const response = await fetch(this.state.result.uri);
+            const blob = await response.blob();
+
+            //uploading the video
+            var ref = firebase.storage().ref().child(result.name);
+            ref.put(blob)
+            .then((res) => {
+
+                //download url
+                firebase.storage().ref(this.state.result.name).getDownloadURL()
+                .then(url => {
+                    this.setState({ url: url });
+
+                    //database commit function
+                    const { currentUser } = firebase.auth();
+                    firebase.database().ref('/videos')
+                    .push({ name: this.state.name, url: this.state.url, userID: currentUser.uid })
+                    .then(() => this.setState({ loading: false }))
+                    .catch((err) => console.log(err));
+
+                    this.initialState();
+                })
+                .catch(err => console.log(err));
+            })
+            .catch(() => this.setState({ loading: false }));
+        }
+        else {
+            console.log("file or name corrupt");
+            this.setState({ loading: false });
+        }
+    }
+
     render() {
         return (
-            <View>
-                <Text style={{ fontSize: 30 }}>
-                    Upload Screen
-                </Text>
+            <View style={{ flex: 1 }} >
+                <Button
+                    title="Select video"
+                    buttonStyle={{
+                        borderRadius: 10,
+                        marginTop: 30
+                    }}
+                    onPress={this.pickVideo}
+                />
+                <FormLabel>Enter Name</FormLabel>
+                <FormInput
+                    onChangeText={name => this.setState({ name })}
+                    value={this.state.name}
+                />
+                <Button
+                    title="upload"
+                    buttonStyle={{
+                        borderRadius: 10,
+                        marginTop: 30
+                    }}
+                    disabled={this.state.uploadButtonDisable}
+                    onPress={this.uploadVideo}
+                />
+                <ModalActivityIndicator
+                    visible={this.state.loading}
+                    size='large'
+                    color='white' 
+                />
             </View>
         );
     }
 }
+
 
 export default Upload;
--- src/screens/VideoWatch.js
+++ src/screens/VideoWatch.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { TabBarBottom, NavigationActions } from 'react-navigation';
+import { Dimensions, View } from 'react-native';
+import Expo, { Video } from 'expo';
 
 class VideoWatch extends Component {
 
     static naigationOptions = {
-        tabBarVisible: false
+        tabBarVisible: false,
+        width: 480,
+        height: 270
+    }
+
+    componentWillMount() {
+        const width = Dimensions.get('window').width;
+        const height = (width*9)/16;
+        this.setState({ width, height });
     }
 
     render() {
         return (
-            <View>
-                <Text style={{ fontSize: 30 }}>
-                    Video Watch Screen
-                </Text>
+            <View style={{ flex: 1 }}>
+                <Video
+                    source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/video-tube-e7c55.appspot.com/o/VID_29970813_232803_670.mp4?alt=media&token=0fcfc75c-d857-42ee-955a-890246930243' }}
+                    rate={1.0}
+                    volume={1.0}
+                    isMuted={false}
+                    resizeMode={Expo.Video.RESIZE_MODE_CONTAIN}
+                    shouldPlay
+                    useNativeControls
+                    style={{ width: this.state.width, height: this.state.height }}
+                />
             </View>
         );
     }
